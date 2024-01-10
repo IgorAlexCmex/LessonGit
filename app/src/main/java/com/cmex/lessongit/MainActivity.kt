653d08a454eff208ad9bdcd46435628a1329ac80
IgorAlexCmex <ia777i888@gmail.com>
2024-01-10 17:50:14 +0300

    change screen background, color size and text size

--- app/src/main/java/com/cmex/lessongit/MainActivity.kt
+++ app/src/main/java/com/cmex/lessongit/MainActivity.kt
@@ -2,10 +2,16 @@ package com.cmex.lessongit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cmex.lessongit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvText.setTextColor(getColor(R.color.white))
+        binding.tvText.textSize="22".toFloat()
+        binding.clMain.setBackgroundResource(R.color.black)
     }
 }
