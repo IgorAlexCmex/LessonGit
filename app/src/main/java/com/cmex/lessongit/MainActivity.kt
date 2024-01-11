05a8231124a7fe1f5dec98458c4d917c2b389527
IgorAlexCmex <ia777i888@gmail.com>
2024-01-11 11:03:21 +0300

    change Background Activity

--- app/src/main/java/com/cmex/lessongit/MainActivity.kt
+++ app/src/main/java/com/cmex/lessongit/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.tvText.setTextColor(getColor(R.color.white))
-        binding.tvText.textSize="22".toFloat()
-        binding.clMain.setBackgroundResource(R.color.black)
+        binding.tvText.textSize=22.0f
+        binding.clMain.setBackgroundResource(androidx.constraintlayout.widget.R.color.material_deep_teal_500)
         binding.tvText.text=getString(R.string.text)
     }
 }
